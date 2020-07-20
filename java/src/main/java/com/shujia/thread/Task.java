b5fd00550fd35a30f856ef234047e4febb6038de
qx <987262086@qq.com>
2020-07-20 15:48:03 +0800

    task

--- java/src/main/java/com/shujia/thread/Task.java
+++ java/src/main/java/com/shujia/thread/Task.java
@@ -1,21 +1,30 @@
 package com.shujia.thread;
 
 import java.io.Serializable;
+import java.sql.Connection;
 
 /**
  * 创建线程
  * <p>
  * 1、计算Thread类或者实现runable接口
- *
- *
+ * <p>
+ * <p>
  * task需要在网络中传输所有需要实现Serializable接口
- *
+ * <p>
  * 序列化
- *      java对象 ---->  字节码数据
- *
+ * java对象 ---->  字节码数据
+ * <p>
  * 反序列化
- *      字节码数据  --->  对象
+ * 字节码数据  --->  对象
+ * <p>
+ * <p>
+ *
+ * 那些东西不能在网络里面传输
+ * JDBC连接
+ * 网络连接
+ * io的对象
  */
+
 
 public class Task extends Thread implements Serializable {
     @Override
@@ -28,6 +37,7 @@ public class Task extends Thread implements Serializable {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
+
 
     }
 }
